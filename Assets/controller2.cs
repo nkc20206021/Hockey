3f4840c73f3f9d12eb205ec1f313b4830cc6398e
nkc20206118 <79066792+nkc20206118@users.noreply.github.com>
2021-06-11 10:28:48 +0900

    Add files via upload

---
+++ Assets/controller2.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller2 : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    private Vector3 pos;
+    bool start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            start = true;
+        }
+        if (start)
+        {
+            if (Input.GetKey("d"))
+            {
+                transform.position += transform.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey("a"))
+            {
+                transform.position -= transform.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (Input.GetKey("w"))
+                {
+                    transform.position += transform.forward * 9f * Time.deltaTime;
+                }
+                if (Input.GetKey("s"))
+                {
+                    transform.position -= transform.forward * 9f * Time.deltaTime;
+                }
+                if (Input.GetKey("d"))
+                {
+                    transform.position += transform.right * 9f * Time.deltaTime;
+                }
+                if (Input.GetKey("a"))
+                {
+                    transform.position -= transform.right * 9f * Time.deltaTime;
+                }
+            }
+        }
+        Clamp();
+        // 追加
+        // プレーヤーの移動できる範囲を制限する命令ブロック
+        void Clamp()
+        {
+            // プレーヤーの位置情報を「pos」という箱の中に入れる。
+            pos = transform.position;
+
+            pos.x = Mathf.Clamp(pos.x, -9.5f, -0.6f);
+            pos.z = Mathf.Clamp(pos.z, -4, 4);
+
+            transform.position = pos;
+        }
+    }
+
+
+
+}
